5e39bf28dd232b2d609af9d8b719edc0eea9a518
Maxired (Maxence Dalmais) <github@maxired.fr>
2015-10-08 10:30:39 +0200

    standardized the module

--- test/fault.js
+++ test/fault.js
@@ -1,10 +1,6 @@
 /* Copyright (c) 2014 Richard Rodger */
-"use strict";
-
-
-// node fault.js
+'use strict'
 
 var test = require('seneca-transport-test')
 
-test.foo_fault( require, 'redis' )
-
+test.foo_fault(require, 'redis')
