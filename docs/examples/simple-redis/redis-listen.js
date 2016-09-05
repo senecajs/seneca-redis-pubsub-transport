582a0ac94659c5c56fdc91e92eb81cf51c32e05b
David Gonzalez <nodearth@gmail.com>
2016-09-05 17:03:10 +0100

    removed console.log

--- docs/examples/simple-redis/redis-listen.js
+++ docs/examples/simple-redis/redis-listen.js
@@ -3,7 +3,6 @@
 let seneca = require('seneca')()
 seneca.use('../../../lib').ready(function () {
   this.add({foo: 'one'}, function (args, done) {
-    console.log('called')
     done(null, {bar: args.bar})
   })
 })
